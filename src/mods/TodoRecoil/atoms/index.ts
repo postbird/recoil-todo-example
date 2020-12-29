bd48d810506c6db7a8dfefe30861447fc444778c
ptbird <ptbird@yeah.net>
2020-12-30 01:31:30 +0800

    feat: init

--- src/mods/TodoRecoil/atoms/index.ts
+++ src/mods/TodoRecoil/atoms/index.ts
@@ -1,48 +1,72 @@
-import { atom, atomFamily, selectorFamily, RecoilState } from 'recoil';
+import { useCallback } from 'react';
+import {
+	atom,
+	atomFamily,
+	selectorFamily,
+	RecoilState,
+	selector,
+	ReadOnlySelectorOptions,
+	useRecoilCallback,
+	useRecoilState,
+} from 'recoil';
 import { v4 as uuidv4 } from 'uuid';
+import { TODO_FILTER } from '../../../constants';
+
+export type TodoId = string | number;
 
 export interface ITodo {
-	id: string;
+	id: TodoId;
 	title: string;
 	completed: boolean;
 	deleted: boolean;
 	created: number;
 }
 
-type ITodoList = ITodo[];
+export type TodoIds = TodoId[];
 
-export const generateTodo = (title: string): ITodo => ({
-	id: uuidv4(),
+export const generateTodo = (title: string) => ({
 	title,
 	completed: false,
 	deleted: false,
 	created: +new Date(),
 });
 
-export const todoListAtom: RecoilState<ITodoList> = atom({
-	key: 'todoList',
-	default: [
-		generateTodo('xx'),
-		generateTodo('xx'),
-		generateTodo('xx'),
-		generateTodo('xx'),
-	] as ITodoList,
+export const todoIDsAtom: RecoilState<TodoIds> = atom({
+	key: 'todoIDsAtom',
+	default: [] as TodoIds,
 });
 
-export const todoItemSelectorWithId = selectorFamily({
-	key: 'todoItemSelectorWithId',
-	get: (id: string) => ({ get }) => get(todoListAtom).filter(item => item.id === id)[0],
-});
-
-export const todoAtomWithId = atomFamily({
+export const todoWithIdAtom = atomFamily({
 	key: `todoItemFamily`,
-	default: (id: string) => todoItemSelectorWithId(id),
+	default: null,
 });
 
-// export const todoAtomFamily = atomFamily((id: string, title: string) => {});
+export const useInsertAtom = useRecoilCallback(({ set }) => (title: string) => {
+	const id = uuidv4();
+	set(todoWithIdAtom(id), generateTodo(title));
+});
 
-// export const todoWithId = (id: string) =>
-// 	atom({
-// 		key: `todoItem${id}`,
-// 		default: null,
-//   });
+export const todoFilterAtom = atom({
+	key: 'todoFilterAtom',
+	default: TODO_FILTER.ALL,
+});
+
+// export const todoListFilterSelector = selector({
+// 	key: 'todoListFilterSelector',
+// 	get: ({ get }) => {
+// 		const filter = get(todoFilterAtom);
+// 		const todoList = get(todoListAtom);
+// 		console.log(todoList);
+
+// 		return todoList.filter(todo => {
+// 			switch (filter) {
+// 				case TODO_FILTER.ACTIVE:
+// 					return !todo.completed;
+// 				case TODO_FILTER.COMPLETED:
+// 					return todo.completed;
+// 				default:
+// 					return true;
+// 			}
+// 		});
+// 	},
+// });
