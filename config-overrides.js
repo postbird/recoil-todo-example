4c2d244fc9ae94b64646b819a3d477dcba120226
ptbird <ptbird@yeah.net>
2021-01-22 00:48:10 +0800

    feat: use cra

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const { override, addWebpackResolve } = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+	addWebpackResolve({
+		alias: {
+			react: path.resolve(__dirname, 'node_modules/react'),
+			'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+		},
+	})
+);
